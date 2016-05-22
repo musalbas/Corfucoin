ff324a134f0182fc159eeff4c745e549b0291f70
Mustafa Al-Bassam <mus@musalbas.com>
2016-05-23 00:36:40 +0100

    Fix main

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0x8e4de98bfe6a6f25cfea04ec4a7408a520b4326c0b26afb9b6389b852c2b47e7");
-static CBigNum bnProofOfWorkLimit = CBigNum().SetCompact(504365644); // Corfucoin: starting difficulty is 1 / 2^12
+uint256 hashGenesisBlock("0x2706e9bba710e55833c14b58e4cd9a2c592cfbb8bb1c943e6978281044900dc2");
+static CBigNum bnProofOfWorkLimit = CBigNum().SetCompact(504365644); // Bestcoin: starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
 uint256 nBestChainWork = 0;
@@ -64,7 +64,7 @@ map<uint256, set<uint256> > mapOrphanTransactionsByPrev;
 // Constant stuff for coinbase transactions we create:
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Corfucoin Signed Message:\n";
+const string strMessageMagic = "Bestcoin Signed Message:\n";
 
 double dHashesPerSec = 0.0;
 int64 nHPSTimerStart = 0;
@@ -355,7 +355,7 @@ unsigned int LimitOrphanTxSize(unsigned int nMaxOrphans)
 
 bool CTxOut::IsDust() const
 {
-    // Corfucoin: IsDust() detection disabled, allows any valid dust to be relayed.
+    // Bestcoin: IsDust() detection disabled, allows any valid dust to be relayed.
     // The fees imposed on each dust txo is considered sufficient spam deterrant. 
     return false;
 }
@@ -617,7 +617,7 @@ int64 CTransaction::GetMinFee(unsigned int nBlockSize, bool fAllowFree,
         }
     }
 
-    // Corfucoin
+    // Bestcoin
     // To limit dust spam, add nBaseFee for each output less than DUST_SOFT_LIMIT
     BOOST_FOREACH(const CTxOut& txout, vout)
         if (txout.nValue < DUST_SOFT_LIMIT)
@@ -1074,13 +1074,13 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 5000000000;
 
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 840000); // Corfucoin: 840k blocks in ~4 years
+    nSubsidy >>= (nHeight / 840000); // Bestcoin: 840k blocks in ~4 years
 
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 2400; // Corfucoin: 3.5 days
-static const int64 nTargetSpacing = 150; // Corfucoin: 2.5 minutes
+static const int64 nTargetTimespan = 2400; // Bestcoin: 3.5 days
+static const int64 nTargetSpacing = 150; // Bestcoin: 2.5 minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 //
@@ -1139,7 +1139,7 @@ unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBl
         return pindexLast->nBits;
     }
 
-    // Corfucoin: This fixes an issue where a 51% attack can change difficulty at will.
+    // Bestcoin: This fixes an issue where a 51% attack can change difficulty at will.
     // Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
     int blockstogoback = nInterval-1;
     if ((pindexLast->nHeight+1) != nInterval)
@@ -2085,7 +2085,7 @@ bool CBlock::CheckBlock(CValidationState &state, bool fCheckPOW, bool fCheckMerk
     if (vtx.empty() || vtx.size() > MAX_BLOCK_SIZE || ::GetSerializeSize(*this, SER_NETWORK, PROTOCOL_VERSION) > MAX_BLOCK_SIZE)
         return state.DoS(100, error("CheckBlock() : size limits failed"));
 
-    // Corfucoin: Special short-term limits to avoid 10,000 BDB lock limit:
+    // Bestcoin: Special short-term limits to avoid 10,000 BDB lock limit:
     if (GetBlockTime() < 1376568000)  // stop enforcing 15 August 2013 00:00:00
     {
         // Rule is: #unique txids referenced <= 4,500
@@ -2242,7 +2242,7 @@ bool CBlock::AcceptBlock(CValidationState &state, CDiskBlockPos *dbp)
 
 bool CBlockIndex::IsSuperMajority(int minVersion, const CBlockIndex* pstart, unsigned int nRequired, unsigned int nToCheck)
 {
-    // Corfucoin: temporarily disable v2 block lockin until we are ready for v2 transition
+    // Bestcoin: temporarily disable v2 block lockin until we are ready for v2 transition
     return false;
     unsigned int nFound = 0;
     for (unsigned int i = 0; i < nToCheck && nFound < nRequired && pstart != NULL; i++)
@@ -2728,7 +2728,7 @@ bool LoadBlockIndex()
         pchMessageStart[1] = 0xc1;
         pchMessageStart[2] = 0xb7;
         pchMessageStart[3] = 0xdc;
-        hashGenesisBlock = uint256("0xa1f040d9e2d577f7fb5026841b68ad2101ff777eecc4383579976ab4e06efa4c");
+        hashGenesisBlock = uint256("0x71bbe0d2c046973c4148fae3bddb24aad87eea8ac98d323ed0c06693d70eab66");
     }
 
     //
@@ -2767,20 +2767,20 @@ bool InitBlockIndex() {
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 5000000000;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("5497c0b6149ac7a846ae80e1d63623a6f1176c34832f072dc45d9d5261051727c13310f6f96a0df4c707f860fc21c40f0bd3590333094d4d537d922ebb6090ed8d") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("4ab2a8b3f5ecc60f08bdebe97235bc3794e5fe6a23d82b4a473664ccc26c892b6e802680baabf2a52753401ed3ce1fffaf1bcdff21dd7d95bae20308c7f35b6102") << OP_CHECKSIG;
         CBlock block;
         block.vtx.push_back(txNew);
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1460159123;
+        block.nTime    = 1463959997;
         block.nBits    = 504365644;
-        block.nNonce   = 2100286412;
+        block.nNonce   = 1397797352;
 
         if (fTestNet)
         {
-            block.nTime    = 1460159123;
-            block.nNonce   = 2097616640;
+            block.nTime    = 1463959997;
+            block.nNonce   = 2428878161;
         }
 
         //// debug print
@@ -2788,7 +2788,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x6aacaaf0f42a65ec7c98d15262556cf9f7235294cf8510cc5c49f9105d340924"));
+        assert(block.hashMerkleRoot == uint256("0x53aaad15904757b319019146c72b4aa50ebf22385a8814366e481913f8f68f94"));
         block.print();
         assert(hash == hashGenesisBlock);
 
@@ -3061,7 +3061,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Corfucoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Bestcoin: increase each by adding 2 to bitcoin's value.
 
 
 void static ProcessGetData(CNode* pfrom)
@@ -4053,7 +4053,7 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
 
 //////////////////////////////////////////////////////////////////////////////
 //
-// CorfucoinMiner
+// BestcoinMiner
 //
 
 int static FormatHashBlocks(void* pbuffer, unsigned int len)
@@ -4460,7 +4460,7 @@ bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
         return false;
 
     //// debug print
-    printf("Corfucoin RPCMiner:\n");
+    printf("Bestcoin RPCMiner:\n");
     printf("proof-of-work found  \n  hash: %s  \ntarget: %s\n", hash.GetHex().c_str(), hashTarget.GetHex().c_str());
     pblock->print();
     printf("generated %s\n", FormatMoney(pblock->vtx[0].vout[0].nValue).c_str());
@@ -4469,7 +4469,7 @@ bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
     {
         LOCK(cs_main);
         if (pblock->hashPrevBlock != hashBestChain)
-            return error("CorfucoinMiner : generated block is stale");
+            return error("BestcoinMiner : generated block is stale");
 
         // Remove key from key pool
         reservekey.KeepKey();
@@ -4483,7 +4483,7 @@ bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
         // Process this block the same as if we had received it from another node
         CValidationState state;
         if (!ProcessBlock(state, NULL, pblock))
-            return error("CorfucoinMiner : ProcessBlock, block not accepted");
+            return error("BestcoinMiner : ProcessBlock, block not accepted");
     }
 
     return true;
